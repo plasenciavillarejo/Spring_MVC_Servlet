32ac09509652c845dcec885c2ee054606cb0d369
josegetronics <jose.plasencia@getronics.com>
2022-07-30 01:09:10 +0200

    Se configura el contexto de spring

---
+++ Spring_MVC/src/main/java/curso/spring/mvc/controller/HomeController.java
@@ -0,0 +1,19 @@
+package curso.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	// Buenas Prácticas.
+	public static final String VISTAHOME = "home";
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String goHome() {
+		return VISTAHOME;
+	}
+	
+}
